09c9f83fa9e21a87d56a865f6e6a3e3296e6287a
ThinkingSlow <anngloves@126.com>
2017-11-21 10:29:00 +0800

    get_h_n.py
    ''' function:输入一个LSTM输出的time-major的3D tesnor（padding过的），根据序列的真实长度找出序列的最后一个时刻的值。
    input1 shape: (seq_len,batch_size,h)
    input2 shape: (batch_size)
    return shape:  (batch_size,h)
    '''

---
+++ get_h_n.py
@@ -0,0 +1,15 @@
+  def get_h_n(self,lstm_y_output ,true_y_length):
+    ''' lstm_y_output: A 3D Tensor of shape(sey_len, batch_size,hidden_units)(list will work)
+      true_y_length:(batch_size)
+    '''
+    hn_list=[]
+    for i in range(self.config.batch_size):
+      i=tf.cast(i,tf.int32)
+
+      last_step=tf.cast(true_y_length[i]-1,tf.int32)
+
+      hn_list.append(lstm_y_output[last_step, i, :])
+      
+    hn_tensor=tf.convert_to_tensor(hn_list)
+    h_n=tf.reshape(hn_tensor,[tf.shape(hn_tensor)[0], tf.shape(hn_tensor)[-1]])         
+    return h_n
