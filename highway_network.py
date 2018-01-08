ae63ae48cca0ac02b0f598862e316c0c6ef76304
ThinkingSlow <anngloves@126.com>
2018-01-08 15:17:32 +0800

    Update highway_network.py
    add linear()

--- highway_network.py
+++ highway_network.py
@@ -1,5 +1,28 @@
 
 # https://github.com/YichenGong/Densely-Interactive-Inference-Network/blob/master/python/my/tensorflow/nn.py
+from tensorflow.contrib.rnn.python.ops.rnn_cell import _linear
+
+def linear(args, output_size, bias, bias_start=0.0, scope=None, squeeze=False, wd=0.0, input_keep_prob=1.0,
+           is_train=None):
+    with tf.variable_scope(scope or "linear"):
+        if args is None or (nest.is_sequence(args) and not args):
+            raise ValueError("`args` must be specified")
+        if not nest.is_sequence(args):
+            args = [args]
+
+        flat_args = [flatten(arg, 1) for arg in args]
+        # if input_keep_prob < 1.0:
+        assert is_train is not None
+        flat_args = [tf.cond(is_train, lambda: tf.nn.dropout(arg, input_keep_prob), lambda: arg)
+                         for arg in flat_args]
+        flat_out = _linear(flat_args, output_size, bias)
+        out = reconstruct(flat_out, args[0], 1)
+        if squeeze:
+            out = tf.squeeze(out, [len(args[0].get_shape().as_list())-1])
+        if wd:
+            add_wd(wd)
+
+    return out
 
 def highway_layer(arg, bias, bias_start=0.0, scope=None, wd=0.0, input_keep_prob=1.0, is_train=None, output_size = None):
     with tf.variable_scope(scope or "highway_layer"):
