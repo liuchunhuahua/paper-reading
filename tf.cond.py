2c506545cf58001fa5d6e6fa56c729da980847f3
ThinkingSlow <anngloves@126.com>
2018-03-23 15:34:32 +0800

    Delete tf.cond.py

--- tf.cond.py
+++
@@ -1,9 +0,0 @@
-
-
-#https://github.com/YichenGong/Densely-Interactive-Inference-Network/blob/master/python/models/DIIN.py
-## Fucntion for embedding lookup and dropout at embedding layer
-def emb_drop(E, x):
-  emb = tf.nn.embedding_lookup(E, x)
-  emb_drop = tf.cond(self.is_train, lambda: tf.nn.dropout(emb, config.keep_rate), lambda: emb)
-  return emb_drop
-# huahua: 忽然觉得利用tf.cond可以使代码变得很整洁。
